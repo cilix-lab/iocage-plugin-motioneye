24057d1fadb8087fdad4701adb500f8e5fea7f88
fulder <misad90@gmail.com>
2021-03-12 12:29:44 +0100

    Add symlink for pip with dash

--- post_install.sh
+++ post_install.sh
@@ -8,11 +8,14 @@ ln -s /usr/local/bin/python2.7 /usr/local/bin/python
 fetch https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
 python get-pip.py
 
+# Create symlinks for pip
+ln -s /usr/local/bin/pip2.7 /usr/local/bin/pip-2.7
+
 # Create symlink for sha1sum to allow file uploading to function
 ln -s /usr/local/bin/shasum /usr/local/bin/sha1sum
 
 # Install motioneye
-pip2.7 install motioneye
+pip-2.7 install motioneye
 
 # Enable motioneye
 sysrc -f /etc/rc.conf motioneye_enable="YES"
